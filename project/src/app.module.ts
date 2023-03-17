30f5703b6c1840414918707ad75284079cf8ff82
yena choi <geniusyena332@gmail.com>
2023-03-17 22:02:50 +0900

    CHORE : typeorm, mysql2 라이브러리 설치 및 db연결 설정
    Changes to be committed:
    	modified:   project/package-lock.json
    	modified:   project/package.json
    	modified:   project/src/app.module.ts

--- project/src/app.module.ts
+++ project/src/app.module.ts
@@ -6,9 +6,30 @@ import { LoggerMiddleware } from './middlewares/logger.middleware';
 import { UsersModule } from './users/users.module';
 import { RoomsModule } from './rooms/rooms.module';
 import { ReservationsModule } from './reservations/reservations.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-    imports: [ConfigModule.forRoot({ isGlobal: true }), UsersModule, RoomsModule, ReservationsModule],
+    imports: [
+        ConfigModule.forRoot({ isGlobal: true }),
+        TypeOrmModule.forRoot({
+            type: 'mysql',
+            host: process.env.MYSQL_HOST,
+            port: 3306,
+            username: process.env.MYSQL_USERNAME,
+            password: process.env.MYSQL_PASSWORD,
+            database: process.env.MYSQL_DATABASE,
+            entities: [],
+            synchronize: true,
+            logging: false,
+            keepConnectionAlive: true,
+            charset: 'utf8mb4_general_ci',
+            timezone: 'Z',
+            cache: false,
+        }),
+        UsersModule,
+        RoomsModule,
+        ReservationsModule,
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
