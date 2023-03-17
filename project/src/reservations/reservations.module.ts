20a07b2aaa61f0a5604e8c72637c340e0d42456c
yena choi <geniusyena332@gmail.com>
2023-03-18 00:33:45 +0900

    CHORE : 각 모듈에 엔티티 파일 import
    Changes to be committed:
    	modified:   project/src/reservations/reservations.module.ts
    	modified:   project/src/rooms/rooms.module.ts
    	modified:   project/src/users/users.module.ts

--- project/src/reservations/reservations.module.ts
+++ project/src/reservations/reservations.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ReservationsService } from './reservations.service';
 import { ReservationsController } from './reservations.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Reservations } from './entities/reservations.entity';
+import { Rooms } from '../rooms/entities/rooms.entity';
+import { Images } from '../rooms/entities/images.entity';
 
 @Module({
-  providers: [ReservationsService],
-  controllers: [ReservationsController]
+    imports: [TypeOrmModule.forFeature([Reservations, Rooms, Images])],
+    providers: [ReservationsService],
+    controllers: [ReservationsController],
 })
 export class ReservationsModule {}
--- project/src/rooms/rooms.module.ts
+++ project/src/rooms/rooms.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { RoomsService } from './rooms.service';
 import { RoomsController } from './rooms.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Rooms } from './entities/rooms.entity';
+import { Images } from './entities/images.entity';
 
 @Module({
-  providers: [RoomsService],
-  controllers: [RoomsController]
+    imports: [TypeOrmModule.forFeature([Rooms, Images])],
+    providers: [RoomsService],
+    controllers: [RoomsController],
 })
 export class RoomsModule {}
