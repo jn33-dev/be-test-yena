bad3f129819189d66cac967542ad91d696d628f1
yena choi <geniusyena332@gmail.com>
2023-03-17 21:02:17 +0900

    CHORE : cookie-parser, express-session, passport 라이브러리 추가 및 main.ts에 설정 추가
    Changes to be committed:
    	modified:   project/package-lock.json
    	modified:   project/package.json
    	modified:   project/src/main.ts

--- project/src/main.ts
+++ project/src/main.ts
@@ -1,6 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import passport from 'passport';
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
 declare const module: any;
 
 async function bootstrap() {
@@ -16,6 +19,19 @@ async function bootstrap() {
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('api', app, document);
 
+    app.use(cookieParser());
+    app.use(
+        session({
+            resave: false,
+            saveUninitialized: false,
+            secrete: process.env.COOKIE_SECRET,
+            cookie: {
+                httpOnly: true,
+            },
+        }),
+    );
+    app.use(passport.initialize());
+    app.use(passport.session());
     await app.listen(port);
     console.log(`Listening on port ${port}`);
 
