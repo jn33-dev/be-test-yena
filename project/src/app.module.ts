b5c5e3109edc9357c5ff2e137753d5408a53f2ca
yena choi <geniusyena332@gmail.com>
2023-03-17 15:30:14 +0900

    FEAT : logger middleware & users, rooms, reservation 모듈 추가
    - logger middleware 추가 및 설정
    - users, rooms, reservation module/controller/service 생성
    Changes to be committed:
    	modified:   src/app.module.ts
    	new file:   src/middlewares/logger.middleware.ts
    	new file:   src/reservations/reservations.controller.spec.ts
    	new file:   src/reservations/reservations.controller.ts
    	new file:   src/reservations/reservations.module.ts
    	new file:   src/reservations/reservations.service.spec.ts
    	new file:   src/reservations/reservations.service.ts
    	new file:   src/rooms/rooms.controller.spec.ts
    	new file:   src/rooms/rooms.controller.ts
    	new file:   src/rooms/rooms.module.ts
    	new file:   src/rooms/rooms.service.spec.ts
    	new file:   src/rooms/rooms.service.ts
    	new file:   src/users/users.controller.spec.ts
    	new file:   src/users/users.controller.ts
    	new file:   src/users/users.module.ts
    	new file:   src/users/users.service.spec.ts
    	new file:   src/users/users.service.ts

--- project/src/app.module.ts
+++ project/src/app.module.ts
@@ -1,11 +1,19 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
+import { UsersModule } from './users/users.module';
+import { RoomsModule } from './rooms/rooms.module';
+import { ReservationsModule } from './reservations/reservations.module';
 
 @Module({
-    imports: [ConfigModule.forRoot()],
+    imports: [ConfigModule.forRoot({ isGlobal: true }), UsersModule, RoomsModule, ReservationsModule],
     controllers: [AppController],
     providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+    configure(consumer: MiddlewareConsumer): any {
+        consumer.apply(LoggerMiddleware).forRoutes('*');
+    }
+}
