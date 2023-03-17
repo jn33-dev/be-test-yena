588e5b49a00488b60ed3a3b747713a1ff49bb984
yena choi <geniusyena332@gmail.com>
2023-03-18 04:27:47 +0900

    FEAT : 회원가입 API 개발
    - API error response status code 및 error message 수정
    - 회원가입 로직 구현
    Changes to be committed:
    	modified:   project/src/users/users.controller.ts
    	modified:   project/src/users/users.service.ts

--- project/src/users/users.controller.ts
+++ project/src/users/users.controller.ts
@@ -19,33 +19,26 @@ export class UsersController {
         description: '회원 가입에 성공하였습니다.',
     })
     @ApiResponse({
-        status: 412,
+        status: 409,
         description: '이미 가입된 e-mail입니다.',
     })
     @ApiResponse({
-        status: 412,
-        description: 'e-mail 형식이 올바르지 않습니다.',
-    })
-    @ApiResponse({
-        status: 412,
-        description: '패스워드가 일치하지 않습니다.',
-    })
-    @ApiResponse({
-        status: 412,
-        description: '패스워드 형식이 일치하지 않습니다.',
-    })
-    @ApiResponse({
-        status: 412,
+        status: 409,
         description: '이미 사용 중인 닉네임입니다.',
     })
     @ApiResponse({
-        status: 400,
-        description: '요청한 데이터 형식이 올바르지 않습니다.',
+        status: 409,
+        description: '패스워드가 일치하지 않습니다.',
     })
+    @ApiResponse({
+        status: 400,
+        description: '요청한 데이터 형식을 확인해주세요.',
+    })
+    // ########## 회원 가입 API ###########
     @Post('signup')
-    signupUser(@Body() data: JoinRequestDto) {
-        this.usersService.createUser(data);
-        return '회원가입';
+    async signupUser(@Body() data: JoinRequestDto) {
+        await this.usersService.createUser(data);
+        return { message: '회원가입에 성공하였습니다.' };
     }
 
     @ApiOperation({ summary: '로그인' })
@@ -55,13 +48,14 @@ export class UsersController {
         type: TokenDto,
     })
     @ApiResponse({
-        status: 412,
+        status: 401,
         description: '닉네임 또는 패스워드를 확인해주세요.',
     })
     @ApiResponse({
         status: 400,
-        description: '로그인에 실패하였습니다.',
+        description: '요청한 데이터 형식을 확인해주세요.',
     })
+    //  ########## 로그인 API ###########
     @Post('login')
     loginUser(@User() user: LoginRequestDto): TokenDto {
         return;
--- project/src/users/users.service.ts
+++ project/src/users/users.service.ts
@@ -1,7 +1,53 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 import { JoinRequestDto } from './dto/join.request.dto';
+import { Users } from './entities/users.entity';
 
 @Injectable()
 export class UsersService {
-    createUser(data: JoinRequestDto) {}
+    constructor(@InjectRepository(Users) private usersRepository: Repository<Users>) {}
+
+    async findUserByEmail(email: string) {
+        const user = await this.usersRepository.findOne({
+            where: { email },
+        });
+        return user;
+    }
+
+    async findUserByNickname(nickname: string) {
+        const user = await this.usersRepository.findOne({
+            where: { nickname },
+        });
+        return user;
+    }
+
+    async createUser(data: JoinRequestDto) {
+        // e-mail 중복 체크
+        const checkEmail = await this.usersRepository.findOne({ where: { email: data.email } });
+        if (checkEmail) {
+            throw new HttpException('이미 가입된 e-mail입니다.', 409);
+        }
+
+        // 닉네임 중복 체크
+        const checkNickname = await this.usersRepository.findOne({
+            where: { nickname: data.nickname },
+        });
+        if (checkNickname) {
+            throw new HttpException('이미 사용 중인 닉네임입니다.', 409);
+        }
+
+        // 비밀번호 확인
+        if (data.password !== data.confirm) {
+            throw new HttpException('패스워드가 일치하지 않습니다.', 409);
+        }
+
+        const hash = await bcrypt.hash(data.password, 12);
+        await this.usersRepository.save({
+            email: data.email,
+            nickname: data.nickname,
+            password: hash,
+        });
+    }
 }
